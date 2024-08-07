b5acf7f116dc16705b6710cb5cb73a4a03f78f0a
Ebs12345678 <subhransuseo18@gmail.com>
2024-08-07 16:54:14 +0530

    16th commit to EDS Landing Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,6 +22,7 @@ export default function RootLayout({ children }) {
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
 
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-0CQNC2QEEK"></Script>
+        {/* Analytics Script */}
         <Script id="GA-script">{`
 window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -31,6 +32,19 @@ window.dataLayer = window.dataLayer || [];
 `}
 
         </Script>
+
+        {/* Conversion Script */}
+
+<Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11139363496"></Script>
+<Script id="AW-script">{`
+            window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-11139363496');
+`}
+  
+</Script>
 
       </head>
       <body className={inter.className}>
