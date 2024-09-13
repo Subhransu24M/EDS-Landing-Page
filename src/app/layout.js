0c6828bda507701e7452a843629e297d7bf80085
Ebs12345678 <subhransuseo18@gmail.com>
2024-09-13 17:34:47 +0530

    17th commit to EDS Landing Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,16 +35,25 @@ window.dataLayer = window.dataLayer || [];
 
         {/* Conversion Script */}
 
-<Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11139363496"></Script>
-<Script id="AW-script">{`
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-11139363496"></Script>
+        <Script id="AW-script">{`
             window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-11139363496');
+            `}
+        </Script>
 
-  gtag('config', 'AW-11139363496');
-`}
-  
-</Script>
+        {/* Google Tag Manager  */}
+        <Script id="GTM-script" strategy="afterInteractive">{`
+                                  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                                  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                                  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                                  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                                  })(window,document,'script','dataLayer','GTM-TTHP2MZV');
+                                `}
+        </Script>
+        {/* End Google Tag Manager  */}
 
       </head>
       <body className={inter.className}>
@@ -52,6 +61,11 @@ window.dataLayer = window.dataLayer || [];
         <Script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></Script>
         <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></Script>
         <Script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></Script>
+
+        {/* Google Tag Manager (noscript)  */}
+        {/* <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTHP2MZV"
+          height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> */}
+        {/* End Google Tag Manager (noscript) */}
       </body>
     </html>
   );
