33ab3b2a424d0c56e2b78c1da2bb4360b91e3067
Ebs12345678 <subhransuseo18@gmail.com>
2024-09-13 17:44:17 +0530

    19th commit to EDS Landing Page

--- src/app/layout.js
+++ src/app/layout.js
@@ -54,6 +54,19 @@ window.dataLayer = window.dataLayer || [];
                                 `}
         </Script>
         {/* End Google Tag Manager  */}
+          
+          {/* MS Clarity Script */}
+        <Script type="text/javascript" id="MSC-clarity">
+          {`
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+             })(window, document, "clarity", "script", "o1wubcrbmn");
+          `}
+        </Script>
+          {/* MS Clarity Script Ends */}
+
 
       </head>
       <body className={inter.className}>
